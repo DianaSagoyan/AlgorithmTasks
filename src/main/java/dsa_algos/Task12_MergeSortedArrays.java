01651426a00efe86cfd538e98a19a9dd2ce1de5b
DianaSagoyan <dianasagoyan@yahoo.com>
2023-03-22 17:11:43 -0400

    Merge sorted arrays

---
+++ src/main/java/dsa_algos/Task12_MergeSortedArrays.java
@@ -0,0 +1,33 @@
+package dsa_algos;
+
+import java.util.Arrays;
+
+public class Task12_MergeSortedArrays {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+
+        int m = 3, n = 3;
+        merge(nums1, m, nums2, n);
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        // Ptr1 for first array Ptr2 for the second array .
+        int ptr1 = m - 1;
+        int ptr2 = n - 1;
+
+        // And move i backwards from end through the array
+        // write the smallest value pointed at by p1 or p2 to nums1.
+        for (int p = m + n - 1; p >= 0; p--) {
+            if (ptr2 < 0) {
+                break;
+            }
+            if (ptr1 >= 0 && nums1[ptr1] > nums2[ptr2]) {
+                nums1[p] = nums1[ptr1--];
+            } else {
+                nums1[p] = nums2[ptr2--];
+            }
+        }
+        System.out.println(Arrays.toString(nums1));
+    }
+}
