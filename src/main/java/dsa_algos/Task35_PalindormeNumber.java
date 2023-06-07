3ec22ea55233e7b79b54f68407f36d9f37e3f6b5
DianaSagoyan <dianasagoyan@yahoo.com>
2023-06-07 13:48:42 -0400

    Palindrome task

---
+++ src/main/java/dsa_algos/Task35_PalindormeNumber.java
@@ -0,0 +1,26 @@
+package dsa_algos;
+
+public class Task35_PalindormeNumber {
+    //Given an integer x, return true if x is a
+    //palindrome
+    //, and false otherwise.
+    public static void main(String[] args) {
+        int num = 121;
+        System.out.println(palindromeNumber(num));
+    }
+
+    public static boolean palindromeNumber(int num){
+        if(num < 0) return false;
+
+        String numStr = String.valueOf(num);
+        int pointer1 = 0;
+        int pointer2 = numStr.length() - 1;
+
+        while (pointer1 < pointer2){
+            if(numStr.charAt(pointer1) != numStr.charAt(pointer2)) return false;
+            pointer1++;
+            pointer2--;
+        }
+        return true;
+    }
+}
