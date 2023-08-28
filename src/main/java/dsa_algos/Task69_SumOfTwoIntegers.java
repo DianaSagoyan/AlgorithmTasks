cefcea75d3aab42d69c2efbdb3bb0067fa221cbc
DianaSagoyan <dianasagoyan@yahoo.com>
2023-08-28 19:14:27 -0400

    Search Insert Position

---
+++ src/main/java/dsa_algos/Task69_SumOfTwoIntegers.java
@@ -0,0 +1,19 @@
+package dsa_algos;
+
+public class Task69_SumOfTwoIntegers {
+    public static void main(String[] args) {
+        //Given two integers a and b, return the sum of the two integers without using the operators + and -.
+        int a = 2, b = 3;
+
+        System.out.println(sumOfTwoIntegers(a,b));
+    }
+
+    public static int sumOfTwoIntegers(int a, int b){
+        while(b != 0){
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+}
