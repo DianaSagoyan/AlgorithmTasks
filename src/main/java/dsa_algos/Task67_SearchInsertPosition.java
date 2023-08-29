04fa7aeff98f79cc51d95d08330457db1f8faf14
DianaSagoyan <dianasagoyan@yahoo.com>
2023-08-29 14:02:57 -0400

    Corrected Search Insert Position

--- src/main/java/dsa_algos/Task67_SearchInsertPosition.java
+++ src/main/java/dsa_algos/Task67_SearchInsertPosition.java
@@ -14,24 +14,14 @@ public class Task67_SearchInsertPosition {
     }
 
     public static int positionOfTarget(int[] nums, int target){
-        int result = 0;
-        if(target < nums[nums.length - 1]){
-            int left = 0;
-            int right = nums.length - 1;
+        int left = 0, right = nums.length - 1;
 
-            while (left < right){
-                int mid = (left + right) / 2;
-                if(target == nums[mid]) {
-                    result = mid;
-                    break;
-                }
-                if(target < nums[mid]) right = mid - 1;
-                if(target > nums[mid]) left = mid + 1;
-            }
-
-        } else {
-            result = nums.length + (target - nums[nums.length - 1] - 1);
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) return mid;
+            if(target < nums[mid]) right = mid - 1;
+            else left = mid + 1;
         }
-        return result;
+        return left;
     }
 }
